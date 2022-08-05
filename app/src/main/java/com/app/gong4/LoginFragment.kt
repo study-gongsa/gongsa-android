3f0a61f3c7b6d85af35dcad08d80a3bb2375c9b2
zjaym1703 <jym971024@gmail.com>
2022-08-05 23:55:22 +0900

    [FEAT] 이메일, 비밀번호 유효성 검사 코드 추기

--- app/src/main/java/com/app/gong4/LoginFragment.kt
+++ app/src/main/java/com/app/gong4/LoginFragment.kt
@@ -1,59 +1,95 @@
 package com.app.gong4
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import com.app.gong4.databinding.FragmentLoginBinding
+import java.util.regex.Pattern
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var emailEditText : EditText
+    lateinit var passwordEditText: EditText
+    lateinit var errorEmailTextView : TextView
+    lateinit var errorPWTextView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        emailEditText = binding.emailEditText
+        passwordEditText = binding.passwordEditText
+
+        errorEmailTextView = binding.validEmailTextView
+        errorPWTextView = binding.validPasswordTextView
+
+        checkEmail()
+        checkPassword()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    fun checkEmail(){
+        emailEditText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(!android.util.Patterns.EMAIL_ADDRESS.matcher(p0.toString()).matches())
+                {
+                    errorEmailTextView.text = String.format(resources.getString(R.string.login_fragment_wrong_answer),"이메일 주소")
+                    emailEditText.background = context!!.resources.getDrawable(R.drawable.custom_error_input)
+                }
+                else
+                {
+                    errorEmailTextView.text = ""
+                    emailEditText.background = context!!.resources.getDrawable(R.drawable.custom_input)
                 }
             }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+    }
+
+    fun checkPassword(){
+        passwordEditText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(!Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,16}$", p0.toString()))
+                {
+                    errorPWTextView.text = String.format(resources.getString(R.string.login_fragment_wrong_answer),"비밀번호")
+                    passwordEditText.background = context!!.resources.getDrawable(R.drawable.custom_error_input)
+                }
+                else
+                {
+                    errorPWTextView.text = ""
+                    passwordEditText.background = context!!.resources.getDrawable(R.drawable.custom_input)
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
     }
 }
