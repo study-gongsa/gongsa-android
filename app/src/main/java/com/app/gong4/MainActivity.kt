46677d4e55befdd275dcc9737139ed1caaa0384f
zjaym1703 <jym971024@gmail.com>
2022-08-12 21:51:54 +0900

    [FEAT] 1. 인터넷 권한, retrofit2 라이브러리 추가 2. toolbar에 textview 추가

--- app/src/main/java/com/app/gong4/MainActivity.kt
+++ app/src/main/java/com/app/gong4/MainActivity.kt
@@ -2,20 +2,50 @@ package com.app.gong4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.app.gong4.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var toolbarTitle: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //toolbar 안 보이도록 설정
+    //    setSupportActionBar(binding.toolbar)
+      //  supportActionBar?.setDisplayShowTitleEnabled(false)
+
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_graph) as NavHostFragment?
-        navHostFragment?.findNavController()
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        toolbarTitle = binding.toolbarTitle
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        toolbarTitle.text = navController.currentDestination?.label.toString()
+    }
+
 }
