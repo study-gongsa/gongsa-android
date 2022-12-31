8800f0cb0589a08a3fdbe45997ebeb38638621e5
zjaym1703 <jym971024@gmail.com>
2023-01-01 02:02:17 +0900

    [FIX] AutoLoginView 인터페이스 삭제

--- app/src/main/java/com/app/gong4/AutoLoginView.kt
+++
@@ -1,8 +0,0 @@
-package com.app.gong4
-
-import com.app.gong4.model.res.ResponseRefreshTokenBody
-
-interface AutoLoginView {
-    fun onValidateSuccess(response: ResponseRefreshTokenBody)
-    fun onValidateFail(code:Int)
-}
